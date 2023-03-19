015ddcf0da84aa2594a8aed046f7f401a12dcbcb
AdibaSjd <adibasjd.id@gmail.com>
2023-03-19 11:29:32 +0000

    font import

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/badBananasDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/badBananasDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
