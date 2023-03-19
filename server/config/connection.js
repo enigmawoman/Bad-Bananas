c9c1d621e968e959497f914ac16cdbbfe5b6f359
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2023-03-19 14:50:08 +0000

    added the styling to the accordian

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-
-
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/badBananasDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
